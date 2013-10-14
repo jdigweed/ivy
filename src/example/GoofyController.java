f36f7a160cb03aba750f3eb2fa95d5375568d1d8
unknown <lichuch@9JX74P1-PE.us.saic.com>
2013-10-14 12:40:43 -0400

    now ivy basically works, gets spring-mvc without all the extra libs

---
+++ src/example/GoofyController.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GoofyController {
+
+	@RequestMapping("/meh")
+	public void stuff() {
+		
+	}
+	
+}
